6a0ee4ceff11c879d22d2210b74f93df512c9629
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-09 14:06:39 -0600

    datosPrimitivos

---
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/primitivos.js
@@ -0,0 +1,44 @@
+
+let nombre = 'Erik Tamayo';
+console.log( nombre );
+
+nombre = 'ErikDev';
+console.log( nombre );
+
+nombre = "FabianDev";
+nombre = `ErikTamayo`
+
+console.log( typeof nombre );
+
+nombre = 123;
+console.log( typeof nombre );
+
+
+let esMarvel = false;
+console.log( typeof esMarvel);
+
+
+let edad = 25;
+console.log( typeof edad);
+
+edad = 25.001;
+console.log( typeof edad);
+
+
+let Javascript
+console.log( typeof Javascript);
+
+let imNull = null;
+console.log(typeof null);
+
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('b');
+
+console.log(typeof symbol1);
+
+
+console.log( symbol1 === symbol2)
+
+
+
