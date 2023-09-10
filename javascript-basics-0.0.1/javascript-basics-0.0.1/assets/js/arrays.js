f8750af8bf97d6ea1a8afdbc307fc12a97ca7e5e
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-09 18:15:46 -0600

    arrays

---
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/arrays.js
@@ -0,0 +1,18 @@
+let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Tigger'];
+console.log({videoJuegos});
+
+console.log( videoJuegos [1]);
+
+
+let arregloCosas = [
+    true,
+    123,
+    'Erik',
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a: 1},
+
+];
+
+console.log({arregloCosas})
