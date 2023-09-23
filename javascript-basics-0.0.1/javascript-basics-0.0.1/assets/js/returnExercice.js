cb981aa0e39db284d6769d5273367898f6882a01
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-22 19:13:58 -0600

    arguments

--- javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/returnExercice.js
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/returnExercice.js
@@ -7,4 +7,15 @@ const crearPersona = ( nombre, apellido ) => ({nombre, apellido})
 
 
 const persona = crearPersona ('Erik', 'Fabian');
-console.log(persona);
+console.log(persona); 
+
+
+function imprimeArgumentos() {
+    console.log(arguments);
+}
+
+const imprimeArgumentos2 = (edad, ...args ) => {
+    console.log({edad, args});
+}
+
+imprimeArgumentos2(10, true, false, 'Erik', 'Tamayo')
