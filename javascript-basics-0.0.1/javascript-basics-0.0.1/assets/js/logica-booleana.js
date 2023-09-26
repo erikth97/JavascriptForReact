f5073b8832711c62a96ace9bad02268190730725
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-26 00:08:46 -0600

    Asignaciones

--- javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/logica-booleana.js
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/logica-booleana.js
@@ -36,3 +36,44 @@ console.log(false || false);
 
 console.log(regresaTrue() || regresaFalse());
 console.log( '4 condiciones', true || true || true || false); //true
+
+
+
+console. warn('Asignaciones');
+
+const soyUndefined = undefined;
+const souNull = null;
+const soyFalso = false;
+
+const a1 = false && 'Hola Mundo' && 150; //?
+const a2 = 'Hola' && soyFalso && true;
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyUndefined || souNull || 'Ya no soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'ya no soy falso de nuevo' || true;
+
+console.log({a1, a2, a3, a4, a5})
+
+if (true || true || true || false ) {
+    console.log('Hacer algo');
+} else {
+    console.log('Hacer otra cosa');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
