42dc2eb53d334b476d93c4d38d6fa939a5caf0b3
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-25 22:38:36 -0600

    elseif

--- javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/if-else.js
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/if-else.js
@@ -10,7 +10,22 @@ if (a >= 10) {
 
 
 
-console.log('Fin del programa');
+//console.log('Fin del programa');
+
+const hoy = new Date();
+let dia = hoy.getDate();
+
+console.log({dia});
+
+if (dia === 0) {
+    console.log('Domingo');
+}else if (dia === 1) {
+    console.log('Lunes')
+} else if (dia === 2){
+    console.log('Martes');
+}else {
+    console.log('No es lunes, martes o domingo...')
+}
 
 
 
