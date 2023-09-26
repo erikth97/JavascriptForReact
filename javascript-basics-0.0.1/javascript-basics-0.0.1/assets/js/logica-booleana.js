a14234b7cf27060c089c8b72191f7c4309b0c011
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-25 23:52:13 -0600

    or

--- javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/logica-booleana.js
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/logica-booleana.js
@@ -17,5 +17,22 @@ console.log(!regresaFalse());
 
 
 console.warn('And'); // true si todos los valores son verdaderos
-console.log(true && true);
-console.log(true && !false);
+console.log(true && true); //true
+console.log(true && !false); //true
+
+console.log('=======')
+console.log( regresaFalse() && regresaTrue()); //false
+console.log(regresaTrue() && regresaFalse()); //false
+
+console.log('==== & =====');
+regresaFalse() && regresaTrue();
+
+console.log( '4 condiciones', true && true && true && false); // false
+
+
+console.warn('OR'); //true
+console.log(true || false);
+console.log(false || false);
+
+console.log(regresaTrue() || regresaFalse());
+console.log( '4 condiciones', true || true || true || false); //true
