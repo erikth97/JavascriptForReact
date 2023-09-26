66b96770c3781ed491d2ff12204e75b9ff60559c
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-25 22:49:32 -0600

    elseIf-exercice

---
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/elseif-excercice.js
@@ -0,0 +1,16 @@
+// imprime el dia de la semana usando Clean Code
+
+dia = 6; // 0: domingo, 1: lunes...
+
+const diasLetras = {
+    0: 'domingo',
+    1: 'lunes',
+    2: 'martes',
+    3: 'miercoles',
+    4: 'jueves',
+    5: 'viernes',
+    6: 'sabado',
+}
+
+// dia de la semana
+console.log( diasLetras[dia])
