11487f3d5ab0d50748d3dd2f04ccfb5270f4d6b7
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-25 22:17:13 -0600

    slice-spread

--- javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/reference-value.js
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/reference-value.js
@@ -7,7 +7,36 @@ console.log({a, b});
 
 
 let erik = {nombre: 'Erik'};
-let ana = erik;
+let ana = {...erik}
 ana.nombre = 'Ana';
 
 console.log({juan, ana})
+
+
+const cambiaNomnre = ({...persona}) => {
+    persona.nombre = 'Tony';
+    return persona;
+}
+
+let peter = { nombre: 'Peter'};
+let tony = cambiaNomnre(peter);
+
+console.log({peter, tony});
+
+
+//Arreglos
+const frutas = ['Manzana', 'Pera', 'Pinia'];
+
+console.time('slice');
+const otrasFrutas = frutas.slice();
+console.timeEnd('slice');
+
+console.time('spread');
+const otrasFrutas2 = [...frutas];
+console.timeEnd('spread');
+
+
+otrasFrutas.push('Mango');
+
+console.log({frutas, otrasFrutas })
+
