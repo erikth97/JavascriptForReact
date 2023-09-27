fbee7eb8980eeebc34520c002d97ff98c23b0be4
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-26 22:56:29 -0600

    ternaryOperator

--- javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/operador-ternario.js
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/operador-ternario.js
@@ -10,3 +10,22 @@ const horaActual = 10;
 
 let horaApertura;
 let mensaje // Abierto o cerrado, abrimos a las xxxx
+
+if (dia === 0 || dia === 6) {
+    console.log('Fin de semana');
+    horaApertura = 9;
+} else {
+    console.log('Dia de semana');
+    horaApertura = 11;
+}
+
+
+if (horaActual >= horaApertura) {
+    mensaje = 'Esta abierto';
+} else {
+    mensaje = `Esta cerrado, hoy abirmos a las ${horaApertura}`;
+}
+
+
+
+console.log({horaApertura})
