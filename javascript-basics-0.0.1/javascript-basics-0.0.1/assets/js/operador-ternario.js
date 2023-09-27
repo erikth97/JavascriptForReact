aa869655bfcd0c62326b96d7dcea14ab08395fd8
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-26 22:49:01 -0600

    ternaryOperator

---
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/operador-ternario.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+// Entra a un sitio web para consultar si esta abierto hoy...
+const dia = 0; // 0 = domingo ....
+const horaActual = 10;
+
+
+let horaApertura;
+let mensaje // Abierto o cerrado, abrimos a las xxxx
