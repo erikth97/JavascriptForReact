1a75ecc2c2fbcd61d4f129dc9d2d6114c6015b68
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-27 21:53:13 -0600

    tomarCartaExtra

--- blackjack-Project/assets/js/juego.js
+++ blackjack-Project/assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C','D','H','S'];
 const especiales = ['A','J','Q','K']
 
+// esta funcion crea un nuevo par
 const crearDeck = () => {
 
     for(let i = 2; i <= 10; i++) {
@@ -22,8 +23,7 @@ const crearDeck = () => {
             deck.push(esp + tipo);
         }
     }
-
-    console.log(deck)
+    //console.log(deck)
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
@@ -31,3 +31,21 @@ const crearDeck = () => {
 
 
 crearDeck();
+
+
+// Esta funcion permite tomar una carta
+const pedirCarta = () => {
+
+    if (deck.length === 0) {
+        throw'No hay cartas en el deck';
+    }
+
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta); // carta debe de ser de la baraja
+    return carta;
+}
+
+// pedir carta
