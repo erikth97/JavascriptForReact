220b54118e723af1844394aad53776b5fba2c7e1
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-09-30 14:52:04 -0600

    optimization

--- blackjack-Project/assets/js/juego.js
+++ blackjack-Project/assets/js/juego.js
@@ -13,19 +13,16 @@
         btnDetener = document.querySelector('#btnDetener'),
         btnNuevo   = document.querySelector('#btnNuevo');
 
-    const divCartasJugador     = document.querySelector('#jugador-cartas'),
-        divCartasComputadora = document.querySelector('#computadora-cartas'),
+    const divCartasJugadores = document.querySelectorAll('.divCartas'),
         puntosHTML = document.querySelectorAll('small');
 
     
     // Esta funcion inicializa el juego
     const inicializarJuego = (numJugadores = 2) => {
-        deck = crearDeck();
+        deck  = crearDeck();
         for(let i = 0; i< numJugadores; i++){
             puntosJugadores.push(0);
         }
-
-        console.log({puntosJugadores})
     }
 
     // Esta función crea un nuevo deck
@@ -61,20 +58,36 @@
                 : valor * 1;
     }
 
+    // Turno 0: = primer jugador y el ultimo sera la computadora
+    const acumularPuntos = (carta, turno) => {
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta(carta);
+        puntosHTML[turno].innerText = puntosJugadores[turno];
+        return puntosJugadores[turno];
+    }
+
+    const crearCarta = (carta, turno) => {
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+        imgCarta.classList.add('carta');
+        divCartasJugadores[turno].append( imgCarta );
+
+    }
+
     // turno de la computadora
     const turnoComputadora = ( puntosMinimos ) => {
 
+        let puntosComputadora = 0;
+
         do {
             const carta = pedirCarta();
+            puntosComputadora = acumularPuntos(carta, puntosJugadores.length -1);
+            crearCarta(carta, puntosJugadores.length -1);
 
-            puntosComputadora = puntosComputadora + valorCarta( carta );
-            puntosHTML[1].innerText = puntosComputadora;
-            
-            // <img class="carta" src="assets/cartas/2C.png">
-            const imgCarta = document.createElement('img');
-            imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
-            imgCarta.classList.add('carta');
-            divCartasComputadora.append( imgCarta );
+            // const imgCarta = document.createElement('img');
+            // imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+            // imgCarta.classList.add('carta');
+            // divCartasComputadora.append( imgCarta );
 
             if( puntosMinimos > 21 ) {
                 break;
@@ -101,15 +114,9 @@
     btnPedir.addEventListener('click', () => {
 
         const carta = pedirCarta();
-        
-        puntosJugador = puntosJugador + valorCarta( carta );
-        puntosHTML[0].innerText = puntosJugador;
-        
-        // <img class="carta" src="assets/cartas/2C.png">
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
-        imgCarta.classList.add('carta');
-        divCartasJugador.append( imgCarta );
+        const puntosJugador = acumularPuntos(carta, 0);
+
+        crearCarta(carta, 0 );
 
         if ( puntosJugador > 21 ) {
             console.warn('Lo siento mucho, perdiste');
