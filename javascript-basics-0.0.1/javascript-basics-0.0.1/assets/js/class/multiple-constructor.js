6b169a0a21ac1c81da1974e60d87541d359a87c0
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-04 01:05:46 -0600

    multiple constructors

---
+++ javascript-basics-0.0.1/javascript-basics-0.0.1/assets/js/class/multiple-constructor.js
@@ -0,0 +1,20 @@
+class Persona {
+
+    static porObjeto({nombre, apellido, pais}) {
+        return new Persona(nombre, apellido, pais);
+    }
+
+    constructor(nombre, apellido, pais) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.pais = pais;
+    }
+
+    getInfo() {
+        console.log(`info: ${this.nombre}, ${this.apellido}, ${this.pais}`);
+    }
+}
+
+const nombre1 = 'erik',
+        apellido1 = 'tamayo',
+        pais1 = 'mexico';
