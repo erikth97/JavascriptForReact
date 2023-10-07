c460dec90dc21fa558232a2f5bab56bd72f9523a
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-07 15:53:40 -0600

    refactoring

---
+++ blackJack-Vite/src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,13 @@
+/**
+ * permite tomar carta
+ * @param {Array<String>} deck es un arreglo de string
+ * @returns {Array<String>} retorna la carta del deck
+ */
+export const pedirCarta = (deck) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw new Error('No hay cartas en el deck');
+    }
+    const carta = deck.pop();
+    return carta;
+}
