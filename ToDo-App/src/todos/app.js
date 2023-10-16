d78ff60c26af22d7eee15ea3fb06e5b8eb40dbd2
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-15 18:47:38 -0600

    Render App

--- ToDo-App/src/todos/app.js
+++ ToDo-App/src/todos/app.js
@@ -1,4 +1,11 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
+
+
+const elementIDs = {
+    TodoList: '.todos-list',
+}
 
 /**
  * 
@@ -6,10 +13,16 @@ import html from './app.html?raw';
  */
 export const App = (elementID) => {
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos(elementIDs.TodoList, todos);
+    }
+
     // Cuando la funcion App() se llama
     (() =>{
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementID).append(app);
+        displayTodos();
     })();
 }
