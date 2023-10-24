c69b53d36cd4cc0209222b3fbd0dbb3c95a44cd5
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-24 10:56:39 -0600

    Callbacks Error

--- advanced-js/src/concepts/12-callBacks.js
+++ advanced-js/src/concepts/12-callBacks.js
@@ -7,10 +7,16 @@ import {heroes} from '../data/heroes';
 export const callbacksComponent = (element) => {
 
     const id = '5d86371f2343e37870b91ef1';
-    findHero(id, (hero) =>{
+    findHero(id, (error, hero) =>{
+        //element.innerHTML = hero?.name || 'No hay heroe';
+        
+        if (error) {
+            element.innerHTML = error;
+            return;
+        }
+        
         element.innerHTML = hero.name;
     });
-
 
 }
 
@@ -18,11 +24,15 @@ export const callbacksComponent = (element) => {
 /**
  * 
  * @param {String} id
- * @param {(hero: Object)=> void} callback
+ * @param {(error: String|Null, hero: Object)=> void} callback
  */
 const findHero = (id, callback) => {
     
     const hero = heroes.find(hero => hero.id === id);
 
+    if (!hero) {
+        callback(`Hero with id ${id} not found`);
+        return; // undefined
+    }
     callback(hero);
 }
