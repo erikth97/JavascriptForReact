c0ccb3346cb4a7a5c0b0f0d2f07aa91101eb956c
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-24 11:10:06 -0600

    Callback Hell

--- advanced-js/src/concepts/12-callBacks.js
+++ advanced-js/src/concepts/12-callBacks.js
@@ -6,18 +6,25 @@ import {heroes} from '../data/heroes';
  */
 export const callbacksComponent = (element) => {
 
-    const id = '5d86371f2343e37870b91ef1';
-    findHero(id, (error, hero) =>{
+    const id1 = '5d86371f2343e37870b91ef1';
+    const id3 = '5d86371f25a058e5b1c8a65e';
+
+    findHero(id1, (error, hero1) =>{
         //element.innerHTML = hero?.name || 'No hay heroe';
-        
         if (error) {
             element.innerHTML = error;
             return;
         }
-        
-        element.innerHTML = hero.name;
-    });
 
+        findHero(id2, (error, hero2) => {
+            if(error) {
+                element.innerHTML = error;
+                return;
+            }
+
+            element.innerHTML = `${hero1.name} / ${hero2.name}`;
+        })
+    });
 }
 
 
