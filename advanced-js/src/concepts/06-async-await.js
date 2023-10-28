5ad081925ce905d717126ddf8e9f37d6103588e6
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-27 22:24:58 -0600

    Show error tu Users / Async - Await

--- advanced-js/src/concepts/06-async-await.js
+++ advanced-js/src/concepts/06-async-await.js
@@ -8,10 +8,16 @@ export const asyncAwaitComponent = async(element) => {
     const id1 = '5d86371fd55e2e2a30fe1cc3';
     const id2 = '5d86371fd55e2e2a30fe1ccb2';
 
-    const hero1 = await findHero (id1);
-    const hero2 = await findHero(id2);
+    try {
+        const hero1 = await findHero (id1);
+        const hero2 = await findHero(id2);
 
-    element.innerHTML = `${name1} / ${name2}`;
+        element.innerHTML = `${name1} / ${name2}`;
+
+    } catch (error) {
+        element.innerHTML = error;
+    }
+
 
 }
 
