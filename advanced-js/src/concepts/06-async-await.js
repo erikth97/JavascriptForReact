f32ff51d1516d59d176dca787130dadb2cec3c12
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-27 22:21:31 -0600

    Async Await

---
+++ advanced-js/src/concepts/06-async-await.js
@@ -0,0 +1,26 @@
+import { heroes } from '../data/heroes'
+/**
+ * 
+ * @param {HTMLDivElement} element
+ */
+export const asyncAwaitComponent = async(element) => {
+
+    const id1 = '5d86371fd55e2e2a30fe1cc3';
+    const id2 = '5d86371fd55e2e2a30fe1ccb2';
+
+    const hero1 = await findHero (id1);
+    const hero2 = await findHero(id2);
+
+    element.innerHTML = `${name1} / ${name2}`;
+
+}
+
+
+const findHero = async(id) => {
+
+    const hero = heroes.find(hero => hero.id === id);
+    if (!hero)
+        throw `Hero not found`;
+
+        return hero;
+}
