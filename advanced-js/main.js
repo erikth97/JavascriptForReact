170de477ab0d4151e63f60984a7c53fea66d2d73
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-27 22:21:27 -0600

    Async Await

--- advanced-js/main.js
+++ advanced-js/main.js
@@ -5,6 +5,7 @@ import { callbacksComponent } from './src/concepts/02-callBacks';
 import { promiseComponent } from './src/concepts/03-promises';
 import { promiseRaceComponent } from './src/concepts/04-promise-race.js';
 import { asyncComponent } from './src/concepts/05-async';
+import { asyncAwaitComponent } from './src/concepts/06-async-await';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -30,4 +31,5 @@ const element = document.querySelector('.card');
 //callbacksComponent(element);
 // promiseComponent(element);
 // promiseRaceComponent(element);
-asyncComponent(element);
+// asyncComponent(element);
+asyncAwaitComponent(element);
