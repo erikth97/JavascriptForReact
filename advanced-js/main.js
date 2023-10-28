27b67fc6bcb9e8e551285bb06dbdd5f6d852aa2c
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-27 23:04:24 -0600

    For Await If Await

--- advanced-js/main.js
+++ advanced-js/main.js
@@ -7,6 +7,7 @@ import { promiseRaceComponent } from './src/concepts/04-promise-race.js';
 import { asyncComponent } from './src/concepts/05-async';
 import { asyncAwaitComponent } from './src/concepts/06-async-await';
 import { asyncAwait2Component } from './src/concepts/07-async-await';
+import { forAwaitComponent } from './src/concepts/08-for-await';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -34,4 +35,5 @@ const element = document.querySelector('.card');
 // promiseRaceComponent(element);
 // asyncComponent(element);
 // asyncAwaitComponent(element);
-asyncAwait2Component(element);
+// asyncAwait2Component(element);
+forAwaitComponent(element);
