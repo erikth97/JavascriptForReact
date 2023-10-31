3583005937728e3d1e9f2183047674cb1185bb6f
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-10-31 14:11:26 -0600

    Render Quote

--- http-app/src/breakingbad/breakingbad-app.js
+++ http-app/src/breakingbad/breakingbad-app.js
@@ -20,6 +20,21 @@ const fetchQuote = async() => {
 export const BreakingbadApp = async(element) => {
     document.querySelector('#app-title').innerHTML = 'Breakingbad App';
     element.innerHTML = 'Loading...';
+    // await fetchQuote();
 
-    const quote = await fetchQuote();
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authoLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authoLabel, nextQuoteButton);
+    }
+
+
+    fetchQuote()
+    .then(data => renderQuote(data))
 }
