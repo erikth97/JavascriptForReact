0fad2b341514021f7f53e3065f6045283b124828
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-02 15:56:10 -0600

    Mappers

--- http-app/src/users/use-cases/load-users-by-page.js
+++ http-app/src/users/use-cases/load-users-by-page.js
@@ -1,10 +1,10 @@
-
-
+import { localhostUserToModel } from "../mappers/localhost-user.mapper";
+import { User } from "../models/user";
 
 /**
  * 
  * @param {Number} page 
- * @returns
+ * @returns {Promise<User[]>}
  */
 export const loadUsersByPage = async(page = 1) => {
 
@@ -12,6 +12,7 @@ export const loadUsersByPage = async(page = 1) => {
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log(data);
+    const users = data.map(localhostUserToModel);
 
+    return users;
 }
