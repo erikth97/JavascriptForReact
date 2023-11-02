e063a3b61d79143ae401568cc73d1c187fb6af3f
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-02 16:03:43 -0600

    store update

--- http-app/src/users/store/users-store.js
+++ http-app/src/users/store/users-store.js
@@ -7,7 +7,11 @@ const state = {
 }
 
 const loadNextPage = async() => {
-    await loadUsersByPage(state.currentPage + 1);
+    const users = await loadUsersByPage(state.currentPage + 1);
+    if (users.length === 0) return;
+
+    state.currentPage += 1;
+    state.users = users;
 }
 
 const loadPreviousPage = async() => {
@@ -29,6 +33,6 @@ export default {
     onUserChanged,
     reloadPage,
 
-    getUser: () => [...state.users],
+    getUsers: () => [...state.users],
     getCurrentPage: () => state.currentPage,
 }
