c4a29bae8a22a836e49e16e75476a167b3118356
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-04 15:57:14 -0600

    Functionality of Buttons

--- http-app/src/users/presentation/render-buttons/render-buttons.js
+++ http-app/src/users/presentation/render-buttons/render-buttons.js
@@ -1,5 +1,7 @@
 import usersStore from '../../store/users-store';
+import { renderTable } from '../render-table/render-table';
 import './render-buttons.css';
+
 /**
  * 
  * @param {HTMLDivElement} element 
@@ -17,4 +19,16 @@ export const renderButtons = (element) => {
     currentPageLabel.innerText = usersStore.getCurrentPage();
 
     element.append(prevButton, currentPageLabel, nextButton);
+
+
+    nextButton.addEventListener('click', async() => {
+        await usersStore.loadNextPage();
+        currentPageLabel.innerText = usersStore.getCurrentPage();
+        renderTable(element);
+    });
+
+    prevButton.addEventListener('click', () => {
+
+    });
+
 }
