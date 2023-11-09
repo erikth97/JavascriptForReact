77fe8f78aab8e16027845ebc4ec365993d2294a9
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-08 22:04:47 -0600

    store

--- http-app/src/users/store/users-store.js
+++ http-app/src/users/store/users-store.js
@@ -7,23 +7,51 @@ const state = {
 }
 
 const loadNextPage = async() => {
-    const users = await loadUsersByPage(state.currentPage + 1);
-    if (users.length === 0) return;
+    const users = await loadUsersByPage( state.currentPage + 1 );
+    if ( users.length === 0 ) return;
 
     state.currentPage += 1;
     state.users = users;
 }
 
 const loadPreviousPage = async() => {
-    throw new Error('No implementado');
+    if ( state.currentPage === 1 ) return;
+    const users = await loadUsersByPage( state.currentPage - 1 );
+    
+    state.users = users;
+    state.currentPage -= 1;
 }
 
-const onUserChanged = () => {
-    throw new Error('No implementado');
+/**
+ * 
+ * @param {User} updatedUser 
+ */
+const onUserChanged = (updatedUser) => {
+
+    let wasFound = false;
+
+    state.users = state.users.map( user => {
+        if ( user.id === updatedUser.id ) {
+            wasFound = true;
+            return updatedUser;
+        }
+        return user;
+    });
+
+    if ( state.users.length < 10 && !wasFound ) {
+        state.users.push( updatedUser );
+    }
+
 }
 
 const reloadPage = async() => {
-    throw new Error('No implementado');
+    const users = await loadUsersByPage( state.currentPage );
+    if ( users.length === 0 ) {
+        await loadPreviousPage();
+        return;
+    } 
+    
+    state.users = users;
 }
 
 
@@ -33,6 +61,13 @@ export default {
     onUserChanged,
     reloadPage,
 
+    /**
+     * @returns {User[]}
+     */
     getUsers: () => [...state.users],
+
+    /**
+     * @returns {Number}
+     */
     getCurrentPage: () => state.currentPage,
 }
